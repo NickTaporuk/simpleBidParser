f1167ef693dad567e74e6abffdf2cfebca7fd399
Nikolay Kuropatkin <nictaporuk@gmail.com>
2018-06-28 09:06:34 +0300

    [simpleBidParser][testing]

--- main_test.go
+++ main_test.go
@@ -4,22 +4,45 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	_ "github.com/stretchr/testify/assert"
 	"simpleBidParser/routes"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"bytes"
 )
 
 func TestIndexHandler(t *testing.T) {
 
-	req, err := http.NewRequest("POST", "/", nil)
-	if err != nil {
-		t.Fatal(err)
+	var TestsDataJson []string
+
+	TestsDataJson = append(
+		TestsDataJson,
+		"./testdata/example-request-app-android-1.json",
+		"./testdata/example-request-app-android-2.json",
+		"./testdata/example-request-web-iphone.json",
+		"./testdata/example-request-web-safari.json",
+		"./testdata/example-request-web-ie8.json")
+
+	for _, data := range TestsDataJson {
+
+		// download test bid request json file
+		raw, err := ioutil.ReadFile(data)
+
+		if err != nil {
+			t.Error("Bid request test file not found")
+		}
+
+		var BidRequestTestDataAndroid = []byte(raw)
+
+		req, err := http.NewRequest("POST", "/", bytes.NewBuffer(BidRequestTestDataAndroid))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(routes.IndexHandler)
+
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t,200, rr.Code,"Ok response is expected")
 	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(routes.IndexHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	assert.Equal(t,200, rr.Code,"Ok response is expected")
 }
--- routes/Index.go
+++ routes/Index.go
@@ -2,49 +2,101 @@ package routes
 
 import (
 	"net/http"
+	"net"
 	"encoding/json"
 	"github.com/mileusna/useragent"
+	"github.com/bsm/openrtb"
+	"github.com/oschwald/geoip2-golang"
+	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 const (
-	DeviceTypeDesktop = "Desktop"
-	DeviceTypeMobile  = "Mobile"
-	DeviceTypeTablet  = "Tablet"
-	DeviceTypeBot     = "Bot"
+	DeviceTypeDesktop    = "Desktop"
+	DeviceTypeMobile     = "Mobile"
+	DeviceTypeTablet     = "Tablet"
+	DeviceTypeBot        = "Bot"
+	MaxMindCountryDbPath = "./maxmind/GeoLite2-Country.mmdb"
 )
 
 type BidParser struct {
 	DeviceType string `json:"device_type"`
-	Os         string `json:"os"`
-	OsVersion  string `json:"os_version"`
+	OS         string `json:"os"`
 	Browser    string `json:"browser"`
 	Country    string `json:"country"`
 	Url        string `json:"url,omitempty"`
 }
 
+func SetDeviceTypeByUserAgent(agent *ua.UserAgent, bp *BidParser) {
+
+	switch {
+	case agent.Mobile:
+		bp.DeviceType = DeviceTypeMobile
+	case agent.Desktop:
+		bp.DeviceType = DeviceTypeDesktop
+	case agent.Tablet:
+		bp.DeviceType = DeviceTypeTablet
+	case agent.Bot:
+		bp.DeviceType = DeviceTypeBot
+	}
+	fmt.Println(agent)
+	bp.OS = agent.OS
+	bp.Browser = agent.Name
+}
+
+func SetUrl(br *openrtb.BidRequest, bp *BidParser) {
+	if br.App != nil {
+		bp.Url = br.App.Domain
+	}
+}
+
+func SetCountryByIp(path string, bp *BidParser, ipClient string) {
+	db, err := geoip2.Open(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	ip := net.ParseIP(ipClient)
+
+	if ip == nil {
+		bp.Country = "localhost"
+	} else {
+
+		record, err := db.Country(ip)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.Country = record.Country.IsoCode
+	}
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	var bidParser BidParser
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+	}
+
+	if body != nil {
+		var req openrtb.BidRequest
+
+		json.Unmarshal([]byte(body), &req)
+
+		SetUrl(&req,&bidParser)
+
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	ua := ua.Parse(r.Header.Get("User-Agent"))
 
-	BidParser := &BidParser{
-		Os:        ua.OS,
-		OsVersion: ua.OSVersion,
-		Browser:   r.Host,
-		Country:   r.URL.Path,
-	}
-
-	if ua.Mobile {
-		BidParser.DeviceType = DeviceTypeMobile
-	} else if ua.Tablet {
-		BidParser.DeviceType = DeviceTypeTablet
-	} else if ua.Desktop {
-		BidParser.DeviceType = DeviceTypeDesktop
-	} else if ua.Bot {
-		BidParser.DeviceType = DeviceTypeBot
-	}
-
-	json, _ := json.Marshal(BidParser)
+	SetDeviceTypeByUserAgent(&ua, &bidParser)
+	SetCountryByIp(MaxMindCountryDbPath,&bidParser, r.RemoteAddr)
+	json, _ := json.Marshal(bidParser)
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(json)
